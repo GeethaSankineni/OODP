de7fc7a91f9774f4aabf55f986d0a395f4c76638
gsankine <gsankine@hawk.iit.edu>
2014-05-24 10:41:57 -0500

    Player panel has been updated

--- OODP/BlackJack/src/com/blackjack/GUI/PlayerPanel.java
+++ OODP/BlackJack/src/com/blackjack/GUI/PlayerPanel.java
@@ -1,25 +1,94 @@
 package com.blackjack.GUI;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Image;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import com.blackjack.bean.Card;
 import com.blackjack.bean.Hand;
 
-public class PlayerPanel {
-	private Hand hand;
-	private int bet;
-	private int money;
-	private boolean isHuman;
-	private int previousOutcome;
-	private JLabel moneyDisp;
-	private JLabel betDisp;
-	public PlayerPanel(String playerName, boolean b, int startMoney,
-			int minBet, Image cardImages) {
-		// TODO Auto-generated constructor stub
-	}
+
+/**
+	 * A container that shows a player's name, remaining cash, and his hand. It also
+	 * contains fields for its current bet, the minimum bet, and the card image file.
+	 * The player can be called upon to do bets, insurance, a game action (for computers),
+	 * receive money/winnings, and clear his hand.
+	 */
+	public class PlayerPanel extends JPanel {
+
+		private static final long serialVersionUID = 1L;
+		
+		private String name;
+		private boolean isHuman;
+		
+		private int money;
+		private int bet;
+		private int minBet;
+		
+		private Hand hand;
+		private Image cardImgs;
+
+		private JLabel moneyDisp;
+		private JLabel betDisp;
+
+		private int previousOutcome = 0;
+
+		/**
+		 * Creates a panel displaying the player's name, his remaining money, and
+		 * his current bet, as well as his hand.
+		 * 
+		 * @param pName
+		 *            Name of the player
+		 * @param isHumanPlayer
+		 *            is the player a human or a computer
+		 * @param startMoney
+		 *            the starting amount of money player has
+		 * @param minimumBet
+		 *            the minimum the player is allowed to bet
+		 * @param cardImages
+		 *            the card images file
+		 */
+		public PlayerPanel(String playerName, boolean isHumanPlayer, int startMoney,int minimumBet, Image cardImages) {
+			
+			super();
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			setPreferredSize(new Dimension(99, 325));
+			setOpaque(false);	
+			
+			Color c = Color.DARK_GRAY;		
+			if (isHumanPlayer)
+				c = Color.LIGHT_GRAY;
+			
+			setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(c), name));
+			
+			this.name = playerName;
+			this.isHuman = isHumanPlayer;
+			this.money = startMoney;
+			this.bet = 0;
+			this.minBet = minimumBet;
+			this.hand = null;
+			this.cardImgs = cardImages;
+			
+			moneyDisp = new JLabel("$" + Integer.toString(money));
+			moneyDisp.setForeground(new Color(87, 233, 100));
+			moneyDisp.setFont(new Font(Font.SERIF, Font.PLAIN, 14));
+			
+			betDisp = new JLabel("$" + Integer.toString(0));
+			betDisp.setForeground(Color.RED);
+			betDisp.setFont(new Font(Font.SERIF, Font.PLAIN, 14));
+
+			add(moneyDisp);
+			add(betDisp);
+		}
+
+	
 
 	public Hand getHand() {
 		// TODO Auto-generated method stub
