3c8891a8025f8a4122ebc6da2649ff268dbae484
gsankine <gsankine@hawk.iit.edu>
2014-05-26 10:36:00 -0500

    Final changes made to  the dealer panel

--- OODP/BlackJack/src/com/blackjack/GUI/DealerPanel.java
+++ OODP/BlackJack/src/com/blackjack/GUI/DealerPanel.java
@@ -1,11 +1,11 @@
 package com.blackjack.GUI;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.util.ArrayList;
 import java.awt.*;
 
 import javax.swing.*;
-
 
 import com.blackjack.bean.Card;
 import com.blackjack.bean.Hand;
@@ -67,7 +67,62 @@ public class DealerPanel extends JPanel {
 		return hand.clearHand();
 	}
 
-
+	/**
+	 * Flips the second card in the hand
+	 */
+	public void flipSecond() {
+		hand.get(1).flip();
+		repaint();
+	}
+	/**
+	 * Paints the cards stacked top-down in addition to the rest of the
+	 * components. The cards are arranged so the user can still see all of the
+	 * cards' values.
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (hand == null)
+			return;
+		for (int i = 0; i < hand.length(); i++) {
+			drawCard(g, hand.get(i), 10, 80 + 33 * i);
+		}
+	}
+	/**
+	 * Paints a card image onto (x,y) of the container. A face down card will be
+	 * drawn accordingly.
+	 * 
+	 * @param g 	- the graphics context
+	 * @param card  - the card to be printed
+	 * @param x 	- the x-position of the printed card in this container
+	 * @param y		- the y-position of the printed card in this container
+	 */
+	private void drawCard(Graphics g, Card card, int x, int y) {
+		int cx; // top-left x of cardsImage
+		int cy; // top-left y of cardsImage
+		if (!card.isFaceUp()) {
+			cx = 2 * 79;
+			cy = 4 * 123;
+		} else {
+			cx = (card.getFace()-1) * 79;
+			switch (card.getSuit()) {
+			case Card.DIAMONDS:
+				cy = 123;
+				break;
+			case Card.CLUBS:
+				cy = 0;
+				break;
+			case Card.HEARTS:
+				cy = 2 * 123;
+				break;
+			default:
+				cy = 3 * 123;
+				break; // Spades
+			}
+		}
+		g.drawImage(cardImgs, x, y, x + 79, y + 123, cx, cy, cx + 79, cy + 123,this);
+	}
+	
 	/**
 	 * Returns the hand of the dealer, probably for comparison purposes.
 	 * 
